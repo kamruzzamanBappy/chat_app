06b73349a8a155b56bbf206c1d6a8b878047e116
kamruzzamanBappy <kamruzzamanbappy13@gmail.com>
2025-06-09 20:36:43 +0600

    d6

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ import cors from "cors";
 import "dotenv/config";
 import express from "express";
 import http from "http";
+import { connectDB } from "./lib/db.js";
+import userRouter from "./routes/userRoutes.js";
 
 //Create Express app & Http server
 const app = express();
@@ -12,7 +14,16 @@ const server = http.createServer(app)
 app.use(express.json({limit: "4mb"}))
 app.use(cors());
 
+
+//Routes setup
 app.use("/api/status", (req,res)=>res.send("Server is Live"));
+app.use("/api/auth",userRouter);
+
+
+//Connect to MONGODB
+await connectDB();
+
+
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, ()=>console.log("Server is running on PORT: "+PORT));

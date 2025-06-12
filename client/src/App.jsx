8f8d5d63ca9e43c3d6081cb08da684c239025c81
kamruzzamanBappy <kamruzzamanbappy13@gmail.com>
2025-06-12 14:03:19 +0600

    d7

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,24 @@
-import { Route, Routes } from "react-router-dom"
+import { useContext } from "react"
+import { Toaster } from "react-hot-toast"
+import { Navigate, Route, Routes } from "react-router-dom"
+import { AuthContext } from "../context/AuthContext"
 import HomePage from "./pages/HomePage"
 import LoginPage from "./pages/LoginPage"
 import ProfilePage from "./pages/ProfilePage"
-
   
+
+//toast use for notification
  const App = () => {
+const {authUser} = useContext(AuthContext)
+
    return (
      <div className="bg-[url('./src/assets/bgImage.svg')]">
+     <Toaster /> 
+     
       <Routes>
-        <Route path='/' element={<HomePage />} />
-  <Route path='/login' element={<LoginPage />} />
-    <Route path='/profile' element={<ProfilePage />} />
+        <Route path='/' element={authUser ?    <HomePage /> : <Navigate to="/login"/>} />
+  <Route path='/login' element={!authUser ?<LoginPage /> : <Navigate to="/"/> } />
+    <Route path='/profile' element={authUser ?<ProfilePage />: <Navigate to="/login"/>} />
 
       </Routes>
      </div>
